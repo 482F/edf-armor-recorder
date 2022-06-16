fe6619c31f17f38a2a98f5df654c07011eac15be
482F <482F.git.mail@482f.net>
2022-06-17 00:00:00 +0900

    表示できるように

---
+++ src/components/main-page.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="main">
+    <InputForm class="input-form" v-model:record="record" />
+    <DataList class="data-list" :record="record" />
+    <ScatterPlot class="scatter-plot" :record="record" />
+  </div>
+</template>
+
+<script setup>
+import InputForm from './input-form.vue'
+import DataList from './data-list.vue'
+import ScatterPlot from './scatter-plot.vue'
+import { ref } from 'vue'
+
+const record = ref(
+  JSON.parse(localStorage.getItem('edf-armor-recorder--record') || '{}')
+)
+</script>
+
+<style lang="scss" scoped>
+.main {
+  padding: 1rem;
+  display: grid;
+  grid-template-rows: 25% 1fr;
+  grid-template-columns: 50% 50%;
+  height: 100vh;
+  width: 100vw;
+  > .input-form {
+    grid-row: 1;
+    grid-column: 1;
+  }
+  > .data-list {
+    grid-row: 1;
+    grid-column: 2;
+  }
+  > .scatter-plot {
+    grid-row: 2;
+    grid-column: 1 / 3;
+  }
+}
+</style>
