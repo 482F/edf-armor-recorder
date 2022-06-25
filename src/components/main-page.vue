e51a04f0ea06e12c549802089e6e44608fc5f86e
482F <0ryonryoko0@gmail.com>
2022-06-25 19:02:42 +0900

    右側に json のテキストエリアを追加

--- src/components/main-page.vue
+++ src/components/main-page.vue
@@ -5,14 +5,12 @@
       :record="record"
       @update:record="updateRecord"
     />
-    <DataList class="data-list" :record="record" />
     <ScatterPlot class="scatter-plot" :record="record" />
   </div>
 </template>
 
 <script setup>
 import InputForm from './input-form.vue'
-import DataList from './data-list.vue'
 import ScatterPlot from './scatter-plot.vue'
 import { ref } from 'vue'
 
@@ -29,22 +27,18 @@ const updateRecord = (newRecord) => {
 <style lang="scss" scoped>
 .main {
   padding: 1rem;
-  display: grid;
-  grid-template-rows: 25% 1fr;
-  grid-template-columns: 50% 50%;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
   height: 100vh;
   width: 100vw;
   > .input-form {
-    grid-row: 1;
-    grid-column: 1;
-  }
-  > .data-list {
-    grid-row: 1;
-    grid-column: 2;
+    flex-grow: 0;
+    flex-shrink: 1;
   }
   > .scatter-plot {
-    grid-row: 2;
-    grid-column: 1 / 3;
+    flex-grow: 1;
+    flex-shrink: 0;
   }
 }
 </style>
